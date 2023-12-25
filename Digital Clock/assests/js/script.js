4fd095b91a0566f8d7f106fed715c9fea836c9fd
amraoui-mohamed <amrmoh023@gmail.com>
2023-12-25 15:03:43 +0100

    init

--- Digital Clock/assests/js/script.js
+++ Digital Clock/assests/js/script.js
@@ -6,8 +6,8 @@ let sec = document.querySelector(".seconds")
 setInterval(() => {
     let currentTime = new Date()
 
-    hour.innerText = currentTime.getHours()
-    min.innerText = currentTime.getMinutes()
+    hour.innerText = (currentTime.getHours()<"10"?"0":"") + currentTime.getHours()
+    min.innerText = (currentTime.getMinutes()<"10"?"0":"") + currentTime.getMinutes()
     sec.innerText = (currentTime.getSeconds()<"10"?"0":"") + currentTime.getSeconds()
 
 }, 1000)
